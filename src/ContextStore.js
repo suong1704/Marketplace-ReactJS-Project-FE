4583fd0687b2b3c9f7c9b0638d3d6dedfb3694e4
suongphan <suongphan37@gmail.com>
2023-06-16 09:10:15 +0700

    done

--- src/ContextStore.js
+++ src/ContextStore.js
@@ -1,30 +1,22 @@
 import React, { useState, useEffect, useMemo } from "react";
-import { TOKEN, getLocalStorage, setLocalStorage } from "./services/helpers";
-// import { useCookies } from 'react-cookie'
+import { getHeader } from "./services/helpers";
 export const Context = React.createContext();
 
 const baseUrl = process.env.REACT_APP_BASE_URL;
 export const ContextStore = ({ children }) => {
   let initialValue = null;
-  // const [cookies, setCookie, removeCookie] = useCookies(['USER_SESSION']);
   const [userData, setUserData] = useState(initialValue);
 
   useEffect(() => {
-    //if (cookies.USER_SESSION) {
     fetch(`${baseUrl}/user/getUser`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${getLocalStorage(TOKEN)}`,
-      },
+      headers: getHeader(),
     })
       .then((res) => res.json())
       .then((res) => {
         return setUserData(res.user);
       });
-    //}
   }, []);
 
-  // console.log(userData)
   const providerValue = useMemo(
     () => ({ userData, setUserData }),
     [userData, setUserData]
--- src/Pages/LogOut.js
+++ src/Pages/LogOut.js
@@ -7,19 +7,20 @@ import {
 const baseUrl = process.env.REACT_APP_BASE_URL;
 
 function LogOut({ history }) {
-  fetch(`${baseUrl}/auth/logout`, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${getLocalStorage(TOKEN)}`,
-    },
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      history.push("/");
-      removeLocalStorage();
-      
-    })
-    .catch((err) => console.log(err));
+  // fetch(`${baseUrl}/auth/logout`, {
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     Authorization: `Bearer ${getLocalStorage(TOKEN)}`,
+  //   },
+  // })
+  //   .then((res) => res.json())
+  //   .then((res) => {
+  //     history.push("/");
+  //     removeLocalStorage();
+  //   })
+  //   .catch((err) => console.log(err));
+  removeLocalStorage();
+  history.push("/");
 }
 
 export default LogOut;
